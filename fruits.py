c07e0f8391abfca0bec473d40089b62bb64f05bd
Danny I <idtdanny@gmail.com>
2025-05-17 11:47:58 +0200

    Fruits Added

--- fruits.py
+++ fruits.py
@@ -1,4 +1,9 @@
 fruits = {"Apple":1000, "Banana": 500, "Mango": 1200}
 
-for fruit in fruits:
-    print(f"{fruit} : ")
+totalCost = 0
+
+for name, price in fruits.items():
+    print(f"{name} : {price}")
+    totalCost += price * 2
+
+print(f"The Total Cost is {totalCost} RWF")
