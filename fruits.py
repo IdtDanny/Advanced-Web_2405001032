784e35d2a86ac103298e700c9fa9a1aa91fcb73f
Danny I <idtdanny@gmail.com>
2025-05-17 11:42:38 +0200

    All pushed

---
+++ fruits.py
@@ -0,0 +1,4 @@
+fruits = {"Apple":1000, "Banana": 500, "Mango": 1200}
+
+for fruit in fruits:
+    print(f"{fruit} : ")
--- lists.py
+++ lists.py
@@ -5,6 +5,8 @@ myList = ["Kagetera", "John", "Peter", "Alex", "Claude"]
 myList.sort(reverse = True)
 
 # Adding appending more than 1 element to list at the same time
-myList.extend(["IDUKUNDATWESE", "Uok", "Danny"])
+myList.append("IDUKUNDATWESE")
+myList.append("University")
+myList.append("Danny")
 
 print(myList)
