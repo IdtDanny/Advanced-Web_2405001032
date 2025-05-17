c7fc9b78d8e410d93ee2cf097ff627881077663d
Danny I <idtdanny@gmail.com>
2025-05-17 10:59:12 +0200

    Adding list

---
+++ lists.py
@@ -0,0 +1,10 @@
+# Create a list
+
+myList = ["Kagetera", "John", "Peter", "Alex", "Claude"]
+
+myList.sort()
+
+# Adding appending more than 1 element to list at the same time
+myList.extend(["IDUKUNDATWESE", "Uok", "Danny"])
+
+print(myList)
